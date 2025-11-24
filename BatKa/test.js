9cd74899c447e121520a9d1fe477c5ee281f2337
Bat-Erdene-B <batka.studies@gmail.com>
2025-11-24 10:02:50 +0800

    o

--- BatKa/test.js
+++ BatKa/test.js
@@ -1,8 +1,31 @@
-const div = document.createElement('div');
+const categoryBtn = document.getElementById('categoryBtn');
+const dropdownMenu = document.getElementById('dropdownMenu')
+const dropdownItems = dropdownMenu.querySelectorAll('.dropdown-item')
 
-div.textContent = 'This div was created by test.js';
-div.innerHTML += '<p style="color: blue;">This is a paragraph inside the div.</p>';
-div.setAttribute('class', 'test-div');
-div.style.border = '2px solid black';
-div.style.padding = '10px';
-div.style.margin = '10px';  
+const container = document.querySelector('.container');
+const LoginLink = document.querySelector('.SignInlink');
+const RegisterLink = document.querySelector('SignUplink');
+
+
+categoryBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    dropdownMenu.classList.toggle('show');
+});
+
+dropdownItems.forEach(item => {
+    item.addEventListener('click', () => {
+        categoryBtn.textContent = item.textContent + ' ';
+        dropdownMenu.classList.remove('show');
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (!dropdownMenu.contains(e.target) && e.target !== categoryBtn) {
+        dropdownMenu.classList.remove('show');
+    }
+});
+
+RegisterLink.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
