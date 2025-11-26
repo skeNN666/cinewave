ec1adc04cd30b4e30c9a8d53cc3cab869a6327f8
Bat-Erdene-B <batka.studies@gmail.com>
2025-11-26 21:35:53 +0800

    a

--- Test by BatKa v2/test.js
+++
@@ -1,8 +0,0 @@
-const div = document.createElement('div');
-
-div.textContent = 'This div was created by test.js';
-div.innerHTML += '<p style="color: blue;">This is a paragraph inside the div.</p>';
-div.setAttribute('class', 'test-div');
-div.style.border = '2px solid black';
-div.style.padding = '10px';
-div.style.margin = '10px';  
